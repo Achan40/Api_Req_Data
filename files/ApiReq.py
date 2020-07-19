ee1b3e5ea835e6f261ed4cd17cca512d3bb08da9
Aaron <51930273+Achan40@users.noreply.github.com>
2020-07-19 11:11:39 -0500

    finished with module

--- files/ApiReq.py
+++ files/ApiReq.py
@@ -2,15 +2,43 @@ import requests
 import pandas as pd
 import os
 
+#required to set your API secret key as SECRET_KEY_TEST
 API_SECRET_KEY = os.environ['SECRET_KEY']
-HTTP_BASE = 'https://cloud.iexapis.com/v1'
-endpoint = 'UNRATE' #Unemployment Rate
+HTTP_BASE = 'https://cloud.iexapis.com/stable'
+HTTP_ID = '/time-series/'
+HTTP_Key = 'economic'
 
-time_range = '2y' #See https://iexcloud.io/docs/api/#time-series for range values
+#this function is used to return a dataframe from an HTTP request
+def json_to_df(HTTP):
+    response = requests.get(HTTP)
+    data = response.json()
+    print(response)
+    IEX_data = pd.DataFrame.from_dict(data)
+    return IEX_data
 
-HTTP_REQUEST = HTTP_BASE+'/time-series/economic/'+endpoint+'?range='+time_range+'&token='+API_SECRET_KEY
-response = requests.get(HTTP_REQUEST)
-data = response.json()
-#IEX_data = pd.DataFrame.from_dict(data)
-#IEX_data.to_csv('test.csv')
+class EconData:
+    def __init__(self):
+        print('Set API secret key as environment variable SECRET_KEY={yourapisecretkeyhere')
+        print('Default http path is ' + HTTP_BASE + HTTP_ID + HTTP_Key)
 
+    def keys(self,HTTP_BASE = HTTP_BASE,HTTP_ID = HTTP_ID):
+        #http request for all HTTP keys under certain ID
+        HTTP = HTTP_BASE + HTTP_ID[0:-1] + '?token=' + API_SECRET_KEY
+        IEX_data = json_to_df(HTTP)
+        #Return endpoint names (all of them)
+        print(IEX_data['id'])
+
+    def endpoints(self,HTTP_BASE = HTTP_BASE,HTTP_ID = HTTP_ID,HTTP_Key = HTTP_Key):
+        #http request for all endpoints under certain HTTP ID and KEY
+        HTTP = HTTP_BASE + HTTP_ID + HTTP_Key + '?token=' + API_SECRET_KEY
+        IEX_data = json_to_df(HTTP)
+        #Return endpoint names (all of them)
+        print(IEX_data['id'])
+
+    def getcsv(self,endpoint,HTTP_BASE = HTTP_BASE,HTTP_ID = HTTP_ID,HTTP_Key = HTTP_Key,time_range='1y'):
+        endpoint = '/'+endpoint
+        HTTP = HTTP_BASE + HTTP_ID + HTTP_Key + endpoint + '?range=' + time_range + '&token=' + API_SECRET_KEY
+        IEX_data = json_to_df(HTTP)
+        endpoint = endpoint[-6:]
+        #outputs csv file into your working directory
+        IEX_data.to_csv(endpoint+'.csv')
